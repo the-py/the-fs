fa2c2e053d34fd521a184a1336de890f82581f76
wenjun.yan <mylastnameisyan@gmail.com>
2013-10-21 20:12:30 +0900

    assert file access

--- thefs.py
+++ thefs.py
@@ -1,65 +1,86 @@
-from os import path as ospath
+import os
 
 
 def basename(self, other):
-    return self._check(ospath.basename(self.obj) == other,
+    return self._check(os.path.basename(self.obj) == other,
                        "Basename of {} is {}.".format(self.obj, other),
                        "Basename of {} is not {}.".format(self.obj, other))
 
 
 def dirname(self, other):
-    return self._check(ospath.dirname(self.obj) == other,
+    return self._check(os.path.dirname(self.obj) == other,
                        "Dirname of {} is {}.".format(self.obj, other),
                        "Dirname of {} is not {}.".format(self.obj, other))
 
 
 def extname(self, other):
-    return self._check(ospath.splitext(self.obj)[1] == other,
+    return self._check(os.path.splitext(self.obj)[1] == other,
                        "Extname of {} is {}.".format(self.obj, other),
                        "Extname of {} is not {}.".format(self.obj, other))
 
 
 def path(self):
-    return self._check(ospath.exists(self.obj),
-                       self.obj + " exist.", self.obj + " exist.", )
+    return self._check(os.path.exists(self.obj),
+                       self.obj + " exist.",
+                       self.obj + " do not exist.", )
 
 
 def file(self):
-    return self._check(ospath.isfile(self.obj),
-                       self.obj + " is a file", self.obj + " is a not file")
+    return self._check(os.path.isfile(self.obj),
+                       self.obj + " is a file",
+                       self.obj + " is a not file")
 
 
 def dir(self):
-    return self._check(ospath.isdir(self.obj),
+    return self._check(os.path.isdir(self.obj),
                        self.obj + " is a directory",
                        self.obj + " is not a directory")
 
 
 def link(self):
-    return self._check(ospath.islink(self.obj),
+    return self._check(os.path.islink(self.obj),
                        self.obj + " is a link",
                        self.obj + " is not a link")
 
 
 def mount(self):
-    return self._check(ospath.ismount(self.obj),
+    return self._check(os.path.ismount(self.obj),
                        self.obj + " is a mount point",
                        self.obj + " is not a mount point")
 
 
 def absolute_path(self):
-    return self._check(ospath.isabs(self.obj),
+    return self._check(os.path.isabs(self.obj),
                        self.obj + " is an absolute path",
                        self.obj + " is not an absolute path")
 
 
-API = [basename, dirname, extname, path, file, dir, link, mount, absolute_path]
+def readable(self):
+    return self._check(os.access(self.obj, os.R_OK),
+                       self.obj + " is readable",
+                       self.obj + " is not readable")
+
+
+def writable(self):
+    return self._check(os.access(self.obj, os.W_OK),
+                       self.obj + " is writable",
+                       self.obj + " is not writable")
+
+
+def executable(self):
+    return self._check(os.access(self.obj, os.X_OK),
+                       self.obj + " is executable",
+                       self.obj + " is not executable")
+
+
+API = [basename, dirname, extname, path, file, dir,
+       link, mount, absolute_path, readable, writable, executable]
 
 # the.use(API)
 
 # p = "/usr/name/file.ext"
-# print ospath.basename(p)
-# print ospath.dirname(p)
+# print os.path.basename(p)
+# print os.path.dirname(p)
 # the(p).should.have.basename("file.ext")
 # the(p).should.have.dirname("/usr/name")
 # p = "./the_fs.py"
